d0b38031c9ac3e2af4351b9e042a3ad286974305
bncer <bncer.ml@gmail.com>
2021-02-11 02:57:42 +0600

    Quick deletion and rename

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -27,7 +27,6 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: <Widget>[
